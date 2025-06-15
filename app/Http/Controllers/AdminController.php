07906554fa61716a11c6a530a129c07758c3b895
developerputul <putulsentongchangya7@gmail.com>
2025-06-15 22:52:21 +0600

    Admin Dashboard Template Setup

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,7 +18,7 @@ class AdminController extends Controller
 
     public function AdminDashboard()
     {
-        return view('admin.admin_dashboard');
+        return view('admin.index');
     } // End Method
 
     public function AdminLoginSubmit(Request $request)
