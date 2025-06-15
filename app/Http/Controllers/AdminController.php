402f8e9bfef56a495613a315d2d0c76c1e711e3a
developerputul <putulsentongchangya7@gmail.com>
2025-06-15 21:15:18 +0600

    Admin Reset and New Confirm password Section Updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -73,4 +73,29 @@ class AdminController extends Controller
         Mail::to($request->email)->send(new Websitemail($subject, $message));
         return redirect()->back()->with('success', 'Reset Password Link Send On Your Email');
     } //End Method
+
+    public function AdminResetPassword($token, $email)
+    {
+        $admin_data = Admin::where('email', $email)->where('token', $token)->first();
+
+        if (!$admin_data) {
+            return redirect()->route('admin.login')->with('error', 'Invalid Token or Email');
+        }
+        return view('admin.reset_password', compact('token', 'email'));
+    } //End Method
+
+    public function AdminResetPasswordSubmit(Request $request)
+    {
+        $request->validate([
+            'password' => 'required',
+            'Password_confirmation' => 'required|same:password',
+        ]);
+
+        $admin_data = Admin::where('email', $request->email)->where('token', $request->token)->first();
+        $admin_data->password = Hash::make($request->password);
+        $admin_data->token = "";
+        $admin_data->update();
+
+        return redirect()->route('admin.login')->with('success', 'Password Reset Successfully');
+    } // End Method
 }
