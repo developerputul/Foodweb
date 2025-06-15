dd372718d04cf7d8848dcce2b9c2cc75af338044
developerputul <putulsentongchangya7@gmail.com>
2025-06-15 18:23:19 +0600

    Admin Reset Password Section Updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use App\Mail\Websitemail;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Mail;
 
 class AdminController extends Controller
 {
@@ -40,5 +43,34 @@ class AdminController extends Controller
     {
         Auth::guard('admin')->logout();
         return redirect()->route('admin.login')->with('success', 'Logout Success');
+    } //End Method
+
+    public function AdminForgetPassword()
+    {
+        return view('admin.forget_password');
+    } // End Method
+
+    public function AdminPasswordSubmit(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $admin_data = Admin::where('email', $request->email)->first();
+        if (!$admin_data) {
+            return redirect()->back()->with('error', 'Email Not Found');
+        }
+        $token = hash('sha256', time());
+        $admin_data->token = $token;
+        $admin_data->update();
+
+        $reset_link = url('admin/reset-password/' . $token . '/' .
+            $request->email);
+        $subject = "Reset Password";
+        $message = "Please Click On below link to reset password<br>";
+        $message .= "<a href=' " . $reset_link . " '>Click Here</a>";
+
+        Mail::to($request->email)->send(new Websitemail($subject, $message));
+        return redirect()->back()->with('success', 'Reset Password Link Send On Your Email');
     } //End Method
 }
--- resources/views/admin/forget_password.blade.php
+++ resources/views/admin/forget_password.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <title>Admin Login</title>
+    <title>Admin Forget Password</title>
 </head>
 <body class="container">
-    <h1>Admin Login</h1>
+    <h1>Admin Forget Password</h1>
 
     @if ($errors->any())
         @foreach ($errors->all() as $error)
@@ -24,22 +24,14 @@
         <li>{{ Session::get('success') }}</li>
     @endif
 
-    <form action="{{ route('admin.login_submit') }}" method="post">
+    <form action="{{ route('admin.password_submit') }}" method="post">
         @csrf
 
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
             <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" name="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-            <a href="{{ route('admin.forget_password') }}">Forget Password</a>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">EMAIL PASSWORD RESET LINK</button>
     </form>
 
 </body>
