4738340b9e8f18474b13c37b48cf6636befffd88
developerputul <putulsentongchangya7@gmail.com>
2025-06-16 14:50:04 +0600

    Admin Dashboard login Section Setup

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -1,46 +1,163 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <title>Admin Login</title>
-</head>
-<body class="container">
-    <h1>Admin Login</h1>
 
-    @if ($errors->any())
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    @endif
+    <head>
 
-    @if (Session::has('error'))
-        <li>{{ Session::get('error') }}</li>
-    @endif
+        <meta charset="utf-8" />
+        <title>Admin Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesbrand" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{asset('Backend/assets/images/favicon.ico')}}">
 
-    @if (Session::has('success'))
-        <li>{{ Session::get('success') }}</li>
-    @endif
+        <!-- preloader css -->
+        <link rel="stylesheet" href="{{asset('Backend/assets/css/preloader.min.css')}}" type="text/css" />
 
-    <form action="{{ route('admin.login_submit') }}" method="post">
-        @csrf
+        <!-- Bootstrap Css -->
+        <link href="{{asset('Backend/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="{{asset('Backend/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="{{asset('Backend/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
 
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" name="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-            <a href="{{ route('admin.forget_password') }}">Forget Password</a>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </head>
 
-</body>
+    <body>
+
+    <!-- <body data-layout="horizontal"> -->
+        <div class="auth-page">
+            <div class="container-fluid p-0">
+                <div class="row g-0">
+                    <div class="col-lg-4 col-lg-4 col-md-4">
+                        <div class="auth-full-page-content d-flex p-sm-5 p-4">
+                            <div class="w-100">
+                                <div class="d-flex flex-column h-100">
+                                    <div class="mb-4 mb-md-5 text-center">
+                                        <a href="index.html" class="d-block auth-logo">
+                                            <img src="{{asset('Backend/assets/images/logo-sm.svg')}}')}}" alt="" height="28"> <span class="logo-txt">Minia</span>
+                                        </a>
+                                    </div>
+                                    <div class="auth-content my-auto">
+                                        <div class="text-center">
+                                            <h5 class="mb-0">Welcome Back !</h5>
+                                            <p class="text-muted mt-2">Sign in to continue to Minia.</p>
+                                        </div>
+
+                    @if ($errors->any())
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    @endif
+
+                    @if (Session::has('error'))
+                        <li>{{ Session::get('error') }}</li>
+                    @endif
+
+                    @if (Session::has('success'))
+                        <li>{{ Session::get('success') }}</li>
+                    @endif
+
+                        <form class="mt-4 pt-2" action="{{ route('admin.login_submit') }}" method="post">
+                            @csrf
+
+                            <div class="mb-3">
+                                <label class="form-label">Email</label>
+                                <input type="email" name="email" class="form-control" id="email" placeholder="Enter Email">
+                            </div>
+                            <div class="mb-3">
+                                <div class="d-flex align-items-start">
+                                    <div class="flex-grow-1">
+                                        <label class="form-label">Password</label>
+                                    </div>
+                                    <div class="flex-shrink-0">
+                                        <div class="">
+                                            <a href="{{ route('admin.forget_password') }}" class="text-muted">Forgot password?</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <div class="input-group auth-pass-inputgroup">
+                                    <input type="password" name="password" class="form-control" placeholder="Enter password" aria-label="Password" aria-describedby="password-addon">
+                                    <button class="btn btn-light shadow-none ms-0" type="button" id="password-addon"><i class="mdi mdi-eye-outline"></i></button>
+                                </div>
+                            </div>
+                            <div class="row mb-4">
+                                <div class="col">
+                                    <div class="form-check">
+                                        
+                                        
+                                    </div>  
+                                </div>
+                                
+                            </div>
+                            <div class="mb-3">
+                                <button class="btn btn-primary w-100 waves-effect waves-light" type="submit">Log In</button>
+                            </div>
+                        </form>
+
+                                <div class="mt-4 pt-2 text-center">
+                                    <div class="signin-other-title">
+                                        <h5 class="font-size-14 mb-3 text-muted fw-medium">- Sign in with -</h5>
+                                    </div>
+
+                                    <ul class="list-inline mb-0">
+                                        <li class="list-inline-item">
+                                            <a href="javascript:void()"
+                                                class="social-list-item bg-primary text-white border-primary">
+                                                <i class="mdi mdi-facebook"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a href="javascript:void()"
+                                                class="social-list-item bg-info text-white border-info">
+                                                <i class="mdi mdi-twitter"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a href="javascript:void()"
+                                                class="social-list-item bg-danger text-white border-danger">
+                                                <i class="mdi mdi-google"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                                <div class="mt-5 text-center">
+                                    <p class="text-muted mb-0">Don't have an account ? <a href="auth-register.html"
+                                            class="text-primary fw-semibold"> Signup now </a> </p>
+                                </div>
+                            </div>
+                            <div class="mt-4 mt-md-5 text-center">
+                                <p class="mb-0">© <script>document.write(new Date().getFullYear())</script> Minia   . Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
+                            </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- end auth full page content -->
+                                    </div>
+                                    <!-- end col -->
+                                
+                                    <!-- end col -->
+                                </div>
+                                <!-- end row -->
+                            </div>
+                            <!-- end container fluid -->
+                        </div>
+
+
+        <!-- JAVASCRIPT -->
+        <script src="{{asset('Backend/assets/libs/jquery/jquery.min.js')}}"></script>
+        <script src="{{asset('Backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{asset('Backend/assets/libs/metismenu/metisMenu.min.js')}}"></script>
+        <script src="{{asset('Backend/assets/libs/simplebar/simplebar.min.js')}}"></script>
+        <script src="{{asset('Backend/assets/libs/node-waves/waves.min.js')}}"></script>
+        <script src="{{asset('Backend/assets/libs/feather-icons/feather.min.js')}}"></script>
+        <!-- pace js -->
+        <script src="{{asset('Backend/assets/libs/pace-js/pace.min.js')}}"></script>
+        <!-- password addon init -->
+        <script src="{{asset('Backend/assets/js/pages/pass-addon.init.js')}}"></script>
+
+    </body>
+
 </html>
