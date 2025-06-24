c00e9831c842374d65c5138b1af884a1796ce59a
developerputul <putulsentongchangya7@gmail.com>
2025-06-24 21:44:47 +0600

    User Change password Section Updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -60,4 +61,36 @@ class UserController extends Controller
         Auth::guard('web')->logout();
         return redirect()->route('login')->with('success', 'Logout Successfully');
     } //End Method
+
+    public function ChangePassword()
+    {
+        return view('frontend.dashboard.change_password');
+    } //End Method
+    public function UserPasswordUpdate(Request $request)
+    {
+
+        $user = Auth::guard('web')->user();
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed',
+        ]);
+        if (!Hash::check($request->old_password, $user->password)) {
+
+            $notification = array(
+                'message' => 'Old Password Dose Not Match',
+                'alert-type' => 'error',
+            );
+            return back()->with($notification);
+        }
+        //Update The New Password
+
+        User::whereId($user->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+        $notification = array(
+            'message' => 'Password Change Successfully',
+            'alert-type' => 'success',
+        );
+        return back()->with($notification);
+    } // End Method
 }
