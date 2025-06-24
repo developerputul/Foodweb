ed6495cf9849478c0a5cf43962d15304a8a14bda
developerputul <putulsentongchangya7@gmail.com>
2025-06-24 21:56:49 +0600

    Update User Login Setup with and without Login

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -19,10 +19,13 @@
                      
                   </li>
                   
-                  @php
-$id = Auth::user()->id;
-    $profileData = App\Models\User::find($id);
-@endphp
+                 
+
+                  @auth
+                   @php
+                  $id = Auth::user()->id;
+                  $profileData = App\Models\User::find($id);
+                  @endphp
 
                   <li class="nav-item dropdown">
                      <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -33,6 +36,22 @@ $id = Auth::user()->id;
                         <a class="dropdown-item" href="{{ route('user.logout') }}"><i class="icofont-sale-discount"></i> Logout</a>
                      </div>
                   </li>
+                  @else
+                  <li class="nav-item dropdown">
+                     <a class="nav-link" href="{{ route('login') }}" role="button" aria-haspopup="true" aria-expanded="false">
+                     Login
+                     </a>
+                  </li>
+
+                  <li class="nav-item dropdown">
+                     <a class="nav-link" href="{{ route('register') }}" role="button" aria-haspopup="true" aria-expanded="false">
+                     Register
+                     </a>
+                  </li>
+                  @endauth
+
+                  
+
                   <li class="nav-item dropdown dropdown-cart">
                      <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                      <i class="fas fa-shopping-basket"></i> Cart
