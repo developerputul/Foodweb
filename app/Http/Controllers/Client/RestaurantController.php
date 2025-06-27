c6e504fe6e57d42330f7c0a249cb1526a725d60e
developerputul <putulsentongchangya7@gmail.com>
2025-06-27 14:03:02 +0600

    Restaurant Product  Add and Product All Section Updated successfully

--- app/Http/Controllers/Client/RestaurantController.php
+++ app/Http/Controllers/Client/RestaurantController.php
@@ -7,9 +7,12 @@ use App\Models\Category;
 use App\Models\City;
 use App\Models\Menu;
 use App\Models\Product;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Intervention\Image\Drivers\Gd\Driver;
 use Intervention\Image\ImageManager;
+use Haruncpi\LaravelIdGenerator\IdGenerator;
+use Illuminate\Support\Facades\Auth;
 
 class RestaurantController extends Controller
 {
@@ -124,5 +127,42 @@ class RestaurantController extends Controller
         return view('client.backend.product.add_product', compact('category', 'menu', 'city'));
     } //End Method
 
-    public function StoreProduct(Request $request) {} //End Method
+    public function StoreProduct(Request $request)
+    {
+        $pcode = IdGenerator::generate(['table' => 'products', 'field' => 'code', 'length' => 5, 'prefix' => 'PC']);
+
+        if ($request->file('image')) {
+            $image = $request->file('image');
+            $manager = new ImageManager(new Driver());
+            $name_gen = hexdec(uniqid()) . '.' .
+                $image->getClientOriginalExtension();
+            $img = $manager->read($image);
+            $img->resize(300, 300)->save(public_path('upload/product/' . $name_gen));
+            $save_url = 'upload/product/' . $name_gen;
+
+            Product::create([
+                'name' => $request->name,
+                'slug' => strtolower(str_replace(' ', '-', $request->name,)),
+                'category_id' => $request->category_id,
+                'city_id' => $request->city_id,
+                'menu_id' => $request->menu_id,
+                'code' => $pcode,
+                'qty' => $request->qty,
+                'size' => $request->size,
+                'price' => $request->price,
+                'discount_price' => $request->discount_price,
+                'client_id' => Auth::guard('client')->id(),
+                'most_popular' => $request->most_popular,
+                'best_seller' => $request->best_seller,
+                'created_at' => Carbon::now(),
+                'status'  => 1,
+                'image' =>  $save_url,
+            ]);
+        }
+        $notification = array(
+            'message' => 'Product Inserted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('all.product')->with($notification);
+    } //End Method
 }
